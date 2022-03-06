4c563db0029a1499a48a92a056b01cfcdbf45729
Igor Golędzinowski <s15538@pjwstk.edu.pl>
2022-03-06 14:20:57 +0100

    Initial commit

---
+++ CrawlerApp/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerApp.Model
+{
+    class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ CrawlerApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using CrawlerApp.Model;
+using System;
+
+namespace CrawlerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zdanie = "Ala ma kota";
+            int? age = null;
+
+            //Console.WriteLine("Hello World!");
+            Student student = new Student();
+            student.Imie = "jan";
+        }
+    }
+}
